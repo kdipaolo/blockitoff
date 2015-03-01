0caae32f96f135207cca0cbabc78af86cecb993d
Kurt DiPaolo <kurtdipaolo@gmail.com>
2015-03-01 15:53:31 -0500

    Getting Task timing to work

--- src/app/todo/todo.controller.js
+++ src/app/todo/todo.controller.js
@@ -5,15 +5,25 @@
 //if you don't use the 'use strict', jS will allow it
 
 
-angular.module('blockitoff')
-.controller('todoCtrl', ["$scope", "$firebase", "$location", function ($scope, $firebase, $location) {
 
-    // ----- Data ------
+
+
+
+
+
+angular.module('blockitoff')
+.controller('todoCtrl', ["$scope", "$firebase", "$location", "$timeout", function ($scope, $firebase, $location, $timeout) {
+
+    // As a user, I want my tasks synced with a persistent backend.
     var todos = [];
     var ref = new Firebase('https://blockitoff.firebaseio.com/todos');
     var sync = $firebase(ref);
     $scope.todos = sync.$asArray();
 
+  
+
+    // As a user, I want to see my active tasks in a list as my default view. [DONE]
+    // As a user, I want to view expired and completed tasks in a view separate from my active tasks. [DONE]
     $scope.statusFilter = {};
 
     if($location.path() == ''){
@@ -27,21 +37,37 @@ angular.module('blockitoff')
           null;
     });
 
-    // ----- ADD ------
+    // As a user, I want to mark tasks complete. 
+    $scope.markComplete = function(){
+      console.log("DONE");
+      sync.child('done').set('true');
+    }
+
+    // As a user, I want to submit new tasks with a description, priority level and three states: expired, completed or active.
     $scope.add = function (item) {
       $scope.todos.$add({text: item, done:false});
       $scope.tab = "";
+      var timeout = $timeout(function(){
+      console.log("REMOVE TASK");
+      }, 5000);
     };
     
-    // ----- DELETE ------
+    // As a user, I want to hide completed tasks [DONE]
     $scope.delete = function (item) {
       $scope.todos.$remove(item);
     }
 
+       // ----- Priority ------
+    $scope.priority = function (item) {
+      // $(this).parent().css('border-left','5px solid orange');
+      // What is the best way to use JQlite within a controller?
+    }
+
+    
+   //tasks older than seven days from my main task views automatically
 
 
+    // Once task is added start timer
+    // once timer reaches seven days remove task to expired 
 
- 
-    // Completed 
-    // AFTER SEVEN DAYS DELETE
   }]);
